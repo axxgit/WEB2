d6c6bb7056754f04a020f506f56a282fe95934b6
axxgit <122202002817@mhs.dinus..ac.id>
2021-04-06 14:04:59 +0700

    text

---
+++ minggu2/latihan/faktorial_for.php
@@ -0,0 +1,46 @@
+<?php
+   $bilangan=$_POST["bilangan"];
+   if($bilangan>=1)
+{
+  echo "Faktorial secara ascending:<br>";
+         
+  for($i=1; $i <= $bilangan; $i++)
+  {
+     $faktorial = 1; //set nilai awal
+     echo $i. "! = "; 
+         
+     for($j=$i; $j > 0; $j--) //karena ascending, nilai awal dimulai dari $i dan tiap kali perulangan dikurang 1
+     {
+    if($j == 1)
+    {
+            echo " 1 = " .$faktorial;
+        }
+    else
+        {            echo $j ." x ";       }
+    ;        $faktorial*=$j; //menghitung hasil faktorial
+     }echo "</br>";
+  }
+}
+else if($bilangan == 0)
+{
+   echo "0! = 1";
+}
+else  //jika memasukkan nilai yang lebih kecil dari 0
+{
+   echo "Anda memasukan bilangan yang salah.";
+}
+?>
+
+<html>
+
+<form action="faktorial_proses.php" method="POST">
+
+  Masukkan suatu bilangan bulat positif : <input name="bilangan" type="text" size="3">
+
+  <br>
+
+  <input name="faktorial" type="submit" value="Hitung">
+
+</form>
+
+</html>
